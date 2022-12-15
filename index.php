0b729f36a6bac99942e93bda77d9aa00fd47a7a6
Vishal8540 <vishalsinghsam8540@gmail.com>
2022-12-15 12:33:49 +0530

    1512

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+// Table from Url 
+
+// $no = 10;
+// $table=$_GET['table'];
+// for($i=1; $i<=$no; $i++){
+// echo  $i*$table;
+// echo  "<br>";
+// }
+
+
+// If Else Condition
+
+// $animal = $_GET['name'];
+
+// $animalsList = [
+//     "Dog","Horse","Tiger","Parrot","Swan",
+// ];
+// if(in_array($animal,$animalsList)){
+//  echo "This is Correct";
+// }else{
+//     echo "this is not Animal";
+// }
+
+
+//Odd Even Checker
+
+// $number = $_GET['number'];
+// if(is_double($number/2)){
+//     echo "Odd Number";
+// }else{
+//     echo "Even Number";
+// }
+
+
+?>
