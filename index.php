71d419fe070bc0ff42cf59f9ba25e61d1b4c3e9b
Vishal8540 <vishalsinghsam8540@gmail.com>
2022-12-17 18:34:37 +0530

    new

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// $connect = mysqli_connect('localhost', 'root', '', 'school');
 // Table from Url 
 
 // $no = 10;
@@ -10,7 +11,7 @@
 // If Else Condition
 
 // $animal = $_GET['name'];
-
+// 
 // $animalsList = [
 //     "Dog","Horse","Tiger","Parrot","Swan",
 // ];
@@ -30,4 +31,44 @@
 //     echo "Even Number";
 // }
 
+// $name = $_GET['name'];
+// $email = $_GET['email'];
+// echo $name .',' .$email;
+// $query = mysqli_query($connect, "INSERT INTO user(name,email)VALUES(
+//     '$name','$email'
+// )");
+
+
+
+
+// class Login{
+//     public $email;
+//     public $password;
+    
+//     public function __construct($email,$password){
+//       $this->email = $email;
+//       $this->password = $password;
+//     }
+    
+//     public function message() {
+//       return "Email:" . $this->email . " Password:". " " . $this->password;
+//     }
+//   }
+  
+  
+//   $user = new Login("Sunil@gmail.com", "123456789");
+//   if (str_ends_with($user->email,"@gmail.com")) {
+//    echo "Correct email";
+//   }
+//   else{
+//     echo "Invalid Email";
+//   }
+
+$number = $_GET['number'];
+if (strlen($number) != 10) {
+ echo "Invalid Number";
+}else{
+    echo "Valid Number";
+}
+  
 ?>
